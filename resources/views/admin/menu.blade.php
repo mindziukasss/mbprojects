bf99384cc56a099f1ff920dda6275ccd0e2d5481
Mindaugas Bernotas <mindaugasbernotas2@gmail.com>
2017-08-27 15:50:07 +0300

    added routes works

--- resources/views/admin/menu.blade.php
+++ resources/views/admin/menu.blade.php
@@ -24,10 +24,10 @@
         <div id="sidebar">
             <ul id="menu" class="nav navbar-nav side-bar">
                 <li class="side-bar tmargin">
-                    <a href="#"><span><i class="fa fa-folder-open-o"></i></span> Projects</a>
+                    <a href="{{ route('app.works.index') }}"><span><i class="fa fa-folder-open-o"></i></span> Projects</a>
                 </li>
                 <li class="side-bar">
-                    <a href="#"><span><i class="fa fa-language" aria-hidden="true"></i>&nbsp;</span> Languages</a>
+                    <a href="{{ route('app.languages.index') }}"><span><i class="fa fa-language" aria-hidden="true"></i>&nbsp;</span> Languages</a>
                 </li>
                 <li class="side-bar">
                     <a href="#"><span><i class="fa fa-file"></i>"&nbsp;</span>Resources</a>
--- routes/web.php
+++ routes/web.php
@@ -33,3 +33,15 @@ Route::group(['prefix' => 'lang'], function () {
     });
 
 });
+
+Route::group(['prefix' => 'works'], function () {
+    Route::get('/', ['as' => 'app.works.index', 'uses' => 'MBWorksController@index']);
+    Route::get('/create', ['as' => 'app.works.create', 'uses' => 'MBWorksController@create']);
+    Route::post('/create', ['uses' => 'MBWorksController@store']);
+    Route::group(['prefix' => '{id}'], function () {
+        Route::get('/', ['as' => 'app.works.show', 'uses' => 'MBWorksController@show']);
+        Route::get('/edit', ['as' => 'app.works.edit', 'uses' => 'MBWorksController@edit']);
+        Route::post('/edit', ['uses' => 'MBWorksController@update']);
+        Route::delete('/delete', ['as' => 'app.works.destroy', 'uses' => 'MBWorksController@destroy']);
+    });
+});
