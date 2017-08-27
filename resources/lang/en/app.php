f3099bc04e431f8de74553afbdd0a8712160419d
Mindaugas Bernotas <mindaugasbernotas2@gmail.com>
2017-08-27 15:47:37 +0300

    change locale language

---
+++ resources/lang/en/app.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ramunes
+ * Date: 2017-08-26
+ * Time: 14:57
+ */
---
+++ resources/lang/lt/app.php
@@ -0,0 +1,19 @@
+<?php
+return [
+    'disable' => 'Išjungti',
+    'active' => 'Įjungti',
+    'language_code' => 'Kalbos kodas',
+    'name' => 'Pavadinimas',
+    'is_active' => 'Išjungti/Įjungti',
+
+
+    'no_data' => "Nėra duomenų",
+    'url' => "Nouroda",
+    'newRecord' => "Naujas įrašas",
+    'show' => "Rodyti",
+    'edit' => "Keisti",
+    'delete' => "Trinti",
+
+    'translation' => 'Vertimas',
+
+];
