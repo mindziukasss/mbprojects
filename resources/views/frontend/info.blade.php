08bf8bfac682b50d1b6a6fbd8d153424e2587a63
Mindaugas Bernotas <mindaugasbernotas2@gmail.com>
2017-09-06 18:14:58 +0300

    created head text slide show

--- resources/views/frontend/info.blade.php
+++ resources/views/frontend/info.blade.php
@@ -2,8 +2,8 @@
 @section('content')
     <div class="jumbotron" id="jumb">
         <div class="container" id="text">
-            <h1>{{trans('app.welcome to my portfolio site')}}</h1>
-            <p>{{trans('app.you can view all my recent work on this site')}}</p>
+            <h1 class="slide">{{trans('app.welcome to my portfolio site')}}</h1>
+            <p class="slide">{{trans('app.you can view all my recent work on this site')}}</p>
         </div>
     </div>
 
--- resources/views/frontend/onepage.blade.php
+++ resources/views/frontend/onepage.blade.php
@@ -14,15 +14,15 @@
 
 </head>
 
-<body>
+<body id="list">
 @include('frontend.menu')
-<div id="list">
+
     @yield('content')
-</div>
+
 
 
 </body>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
 
