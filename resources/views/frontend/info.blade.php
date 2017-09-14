7e27b2b74fd8fdb8fd9daaabc53bf2f0fe7ed8fe
Mindaugas Bernotas <mindaugasbernotas2@gmail.com>
2017-09-14 20:28:54 +0300

    otimize scroll page code

--- resources/views/frontend/info.blade.php
+++ resources/views/frontend/info.blade.php
@@ -151,7 +151,6 @@
                             $('input , textarea').val(function () {
                                 return this.defaultValue;
                             });
-
                         } else {
                             $('#msg').html('<div class="alert alert-danger messages col-xs-6 col-md-4">{{trans('app.error message')}}</div>');
                         }
@@ -163,47 +162,18 @@
                 });
             });
         });
-
         var offsetHeight = 51;
-
-        $('body').scrollspy({
-            offset: offsetHeight
-        });
-
-        // Select all links with hashes
+        $('body').scrollspy({offset: offsetHeight});
         $('a[href*="#"]')
-        // Remove links that don't actually link to anything
-            .not('[href="#"]')
-            .not('[href="#0"]')
-            .click(function(event) {
-                // On-page links
-                if (
-                    location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
-                    &&
-                    location.hostname == this.hostname
-                ) {
-                    // Figure out element to scroll to
-                    var target = $(this.hash);
-                    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-                    // Does a scroll target exist?
-                    if (target.length) {
-                        // Only prevent default if animation is actually gonna happen
-                        event.preventDefault();
-                        $('html, body').animate({
-                            scrollTop: target.offset().top
-                        }, 2500, function() {
-                            // Callback after animation
-                            // Must change focus!
-//                            var $target = $(target);
-//                            $target.focus();
-//                            if ($target.is(":focus")) { // Checking if the target was focused
-//                                return false;
-//                            } else {
-//                                $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-//                                $target.focus(); // Set focus again
-//                            };
-                        });
-                    }
+            .click(function (event) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    event.preventDefault();
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 2500, function () {
+                    });
                 }
             });
 
